1a298ac2873d1f30569cebe27ee0385a9ca1d025
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-02-20 21:05:14 -0600

    Finished working on the repo model, started commit and branch
    I need to finsih both of the commit and branch models so I can finish
    the for-each-ref command and in turn finsih the branch command. I also
    took the code from the github desktop project for the commit id since
    they parse the time exactly like I should do it, plus I'm sure theirs
    works better than the one I would have done :P
    
    # On branch master
    # Your branch is up-to-date with 'origin/master'.
    #
    # Changes to be committed:
    #	deleted:    src/GitDrive/index.ts
    #	renamed:    src/GitDrive/GCore.ts -> src/git-drive/core-git.ts
    #	new file:   src/git-drive/index.ts
    #	renamed:    src/GitDrive/init.ts -> src/git-drive/init.ts
    #	new file:   src/model/branch.ts
    #	new file:   src/model/commit.ts
    #	new file:   src/model/committer-id.ts
    #	renamed:    src/Model/Repo.ts -> src/model/repository.ts
    #	modified:   tsconfig.json
    #
    # Untracked files:
    #	src/git-drive/for-each-ref.ts
    #

--- src/git-drive/init.ts
+++ src/git-drive/init.ts
@@ -1,11 +1,11 @@
 import {IGitResult} from "dugite";
-import {git} from "./GCore";
+import {git} from "./core-git";
 
 /**
  * Init a repo at given path, TODO: add template dir option or set it up
  * using init.template config, we'll see.
  * @param pathToRep
  */
-export default async function name(pathToRep: string): Promise<IGitResult> {
+export default async function init(pathToRep: string): Promise<IGitResult> {
     return await git(["init"], pathToRep);
 }
