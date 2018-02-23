1f55a40b3a9c612f2ca772ead596178776e50721
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-02-23 16:09:47 -0600

    Added remote along with exports in index.ts
    So this is also a basic implementation of remote for now.
    Not much will probably change but we will see. Time to write some tests
    
    # On branch master
    # Your branch is ahead of 'origin/master' by 1 commit.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   src/git-drive/index.ts
    #	modified:   src/git-drive/remote.ts
    #

--- src/git-drive/index.ts
+++ src/git-drive/index.ts
@@ -1,3 +1,7 @@
 export * from "./core-git";
 export * from "./init";
 export * from "./for-each-ref";
+export * from "./remote";
+export * from "./branch";
+export * from "./clone";
+// export * from "./commit";
---
+++ src/git-drive/remote.ts
@@ -0,0 +1,60 @@
+import {Repository} from "../model/repository";
+import {git} from "./core-git";
+
+/** Define an immutable remote object */
+export interface IRemote {
+    readonly name: string;
+    readonly url: string;
+}
+/**
+ * Looks up and returns the one and only remote
+ * This function assumes that the repo added is both and push and fetch repo.
+ * In fact if it is not a fetch and not a push repo the function won't notice,
+ * keep this in mind.
+ * TODO: maybe add a check for this, for now it is not needed as it is a fair
+ * assumption.
+ * @param repo The repo in which the remote belongs to
+ * @returns IRemote or null if the remote doesn't exist
+ */
+export async function getRemote(
+    repo: Repository,
+): Promise<IRemote | null> {
+    // This will return the remotes in the following format:
+    // [remoteName]  [URL] [(fetch || push)]
+    const res = await git(["remote", "-v"], repo.path);
+    const stdout = res.stdout;
+    // return only the lines that end with fetch, arbitrary choice
+    const line = stdout.split("\n").filter( (str) => {
+        return str.endsWith("(fetch)");
+    });
+    // TODO: Log this error in a proper way.
+    if (line.length > 1) {
+        throw new Error("[getRemote: found more than one remote repo");
+    } else if (line.length === 0 ) {
+        return null;
+    }
+    // match one space or more. There should be exactly two
+    const remoteVars = line[0].split(/\s+/);
+    return {name: remoteVars[0], url: remoteVars[1]};
+}
+
+/**
+ * Add the one and only repo.
+ * The git process will reject the promise with an error.
+ * @param repo The repo at which to add the remote
+ * @param url url of the remote repo
+ */
+export async function addRemote(
+    repo: Repository,
+    url: string,
+): Promise<void> {
+    await git(["remote", "add", "origin", url], repo.path);
+}
+
+export async function changeUrl(
+    repo: Repository,
+    url: string,
+): Promise<void> {
+    await git(["remote", "set-url", "origin", url], repo.path);
+}
+
