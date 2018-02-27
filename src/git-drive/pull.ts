7b3dc970667ae22966786d42c54933534b220a39
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-02-26 18:04:00 -0600

    Very basic implemntation of push/pull, baiscally figured out the options
    I also realized another potential problem. People can commit using
    GitHub, very bad and I wrote a shpeal about it in pull. We will have a
    bad time trying to deal with that or most likely not be able to deal
    with it. Baiscally if anyone commits on GitHub and breaks our rules we
    are in trouble. Progress should come really soon, I just want to finish
    a skeleton of most of the model by end of next week.
    
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	new file:   src/git-drive/pull.ts
    #	new file:   src/git-drive/push.ts
    #
    # Untracked files:
    #	src/git-drive/fetch.ts
    #

---
+++ src/git-drive/pull.ts
@@ -0,0 +1,33 @@
+import {Repository} from "../model/repository";
+import {git, overrideCredentialHelper} from "./core-git";
+
+/**
+ * Pull current HEAD.
+ * This is just here for the possibility of needing it. The main problem
+ * is that users can make commits through GitHub, commits that won't follow
+ * our rules and we have to deal with it. We will see. We maybe able to
+ * tolerate the owner of the branch rebasing some remote commits made on
+ * their own branch.
+ * The caller has to verify that the owner of the branch is calling this.
+ * This assumes that the origin is fully configured.
+ * TODO: progress
+ * TODO: authentication
+ * @param repo Repo where to pull the current checked-out branch
+ */
+export async function pull(
+    repo: Repository,
+): Promise<void> {
+    const args = [...overrideCredentialHelper,
+            "pull",
+             "--no-rebase",
+            "origin"];
+    await git(args, repo.path);
+}
+
+
+// No pull-all function as it will automatically break the rule of only the
+// owner of the branch commits on their branch. It will be the case where
+// person 1 made a commit on branch person 2 and person 3 was the one who
+// pulled that commit. Will have to look into this.
+
+
---
+++ src/git-drive/push.ts
@@ -0,0 +1,47 @@
+import {Repository} from "../model/repository";
+import {git, overrideCredentialHelper } from "./core-git";
+
+/**
+ * Push the certain branch to remote repo origin, the one and only remote.
+ * This will also set-up-upstream for the local branch if it is not setup.
+ * We should always setup the up-streams once a remote is added.
+ * Also this will rely on the value under config for the remote url info,
+ * check the documentation for push for more on this.
+ * TODO: add progress callbacks
+ * TODO: figure out authentication.
+ * TODO: error logging.
+ * @param repo repo where to perform the push
+ * @param localBranch the local branch to push
+ */
+export async function pushBranch(
+    repo: Repository,
+    localBranch: string,
+
+): Promise<void> {
+    const args = [...overrideCredentialHelper,
+        "push",
+        "-u",
+        "origin",
+        localBranch];
+
+    await git(args, repo.path);
+}
+
+/**
+ * Push all the branches to remote origin, the one and only.
+ * TODO: refer to the pushBranch todos.
+ * @param repo repo where to push all the branches
+ */
+export async function pushAll(
+    repo: Repository,
+): Promise<void> {
+    const args = [...overrideCredentialHelper,
+    "push",
+    "--all",
+    "-u",
+    "origin"];
+
+    await git(args, repo.path);
+}
+
+
