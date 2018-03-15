6d92c5babe3a532ba4bd339b966f889934153da1
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-03-15 16:06:59 -0500

    Made a decision to include undefined into the return type of getVal
    So this will enforce the called to check whether the value they passed
    for the key exists without them needing to use try and catch. If this
    wasn't the case the caller has might forget that the function could
    error out as the type checks out until it does in runtime. I do not
    think I'll need the function without the undefined as a return type, if
    it I do, I will just make the current one be warpper to the original.

--- src/util/getVal.ts
+++ src/util/getVal.ts
@@ -1,5 +1,9 @@
 
-
-export function getVal<T, K extends keyof T>(arr: T, key: K): T[K] {
-    return arr[key];
+/**
+ * Generic helper function that will return the value from a key iff the key
+ * type is an index of T and the value for the key exists, returns undefined
+ * otherwise.
+ */
+export function getVal<T, K extends keyof T>(arr: T, key: K): T[K] | undefined {
+    return arr[key] !== undefined ? arr[key] : undefined;
 }
