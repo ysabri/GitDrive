f6ee45b6073cd8a1741e4ecb2a31962db5d5b29f
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-04-11 17:52:11 -0500

    WIP load repo works fully (I think) except for the latestState part
    For now we don't get the lateset state. I will fix it tomorrow

--- src/git-drive/app/load-repo.ts
+++ src/git-drive/app/load-repo.ts
@@ -1,11 +1,12 @@
 import { existsSync } from "fs";
+import { changeWS } from "../../controller/state-updater";
 import { GRepository } from "../../model/app/g-repository";
 import { IChangeList, WorkSpace } from "../../model/app/workspace";
 import { Branch } from "../../model/git/branch";
 import { Commit } from "../../model/git/commit";
 import { Repository } from "../../model/git/repository";
 import { readRepoInfo } from "../../util/metafile";
-import { getBranches, getCommit, isGitRepository, getStatus } from "../git";
+import { checkoutBranch, getBranches, getCommit, isGitRepository } from "../git";
 
 export async function loadGRepo(
     path: string,
@@ -24,7 +25,7 @@ export async function loadGRepo(
     // get all the local branches
     // TODO: figure out how to read remote ones that don't have a local
     // counterpart. This shouldn't be the case so I might just not worry.
-    let refs = await getBranches(repo, "refs/heads");
+    const allRefs = await getBranches(repo, "refs/heads");
     let emptyChangeList: IChangeList = {};
     // the first dimension is the topic space, the second is the workspace
     const topicSpaces: Commit[][] = [];
@@ -32,6 +33,10 @@ export async function loadGRepo(
     let topicSpaceCounter: number = 0;
     // Used to check if a branch matches our branch naming schema
     const branchNamePattern = new RegExp(`^G[0-9a-f]{10}$`);
+    const metaBranchPattern = new RegExp(`^GG$`);
+    let refs = allRefs.filter((value) => {
+        return !metaBranchPattern.test(value.name);
+    });
     while (refs.length > 0) {
         // this is based on the fact that git does not allow
         // duplicated branch names, meaning only one branch will get filtered
@@ -41,7 +46,7 @@ export async function loadGRepo(
 
         const refRes = branchNamePattern.test(refs[0].name);
         if (!refRes) {
-            throw new Error(`[loadRepo] ref: ${refs[0]} has an incorrect name`);
+            throw new Error(`[loadRepo] ref: ${refs[0].toPrint()}, has an incorrect name`);
         }
 
         emptyChangeList = {};
@@ -62,7 +67,7 @@ export async function loadGRepo(
         for (const inRef of resetOfRefs) {
             const inRefRes = branchNamePattern.test(inRef.name);
             if (!inRefRes) {
-                throw new Error(`[loadRepo] ref: ${inRef} has an incorrect name`);
+                throw new Error(`[loadRepo] ref: ${inRef.toPrint()}, has an incorrect name`);
             }
 
             emptyChangeList = {};
@@ -91,11 +96,89 @@ export async function loadGRepo(
         }
         topicSpaceCounter++;
     }
-    const statusofCurrBranch = await getStatus(repo);
-    console.log(statusofCurrBranch);
+    await checkoutBranch(repo, "GG");
     const repoInfo = await readRepoInfo(repo);
     console.log("this is the info read about the repo");
     console.log(repoInfo.id());
 
-    return new GRepository(repo.path, [], [], "GG");
+    // This will throw if anything doesn't checkout
+    await checkTheState(repoInfo, topicSpaces);
+
+    return await getThelatestState(repoInfo, refs);
+}
+
+async function checkTheState(
+    repo: GRepository,
+    commits: Commit[][],
+): Promise<void> {
+    if (commits.length !== repo.topicSpaces.length) {
+        throw new Error("The number of topicspace found does't match with" +
+            "what the meta data says");
+    }
+
+    const match: boolean[] = [];
+    for (const TS of repo.topicSpaces) {
+        for ( const suspectedWS of commits) {
+            // This means we can compare them but they might not match since
+            // different WSs could have the same length.
+            if (suspectedWS.length === TS.users.length) {
+                // This is checking that for every value in the suspected WS
+                // commits array one element matches in terms of the author,
+                // this works given the uniqueness of the users and that the
+                // lengths match.
+                const usersMatch = suspectedWS.every((value) => {
+                    return TS.users.some((user) => {
+                        return value.committer.name === user.name;
+                    });
+                });
+                // check if the users are unique in the suspected WS commits
+                // arr. We don't check ours cause we won't write the
+                // proto repo file otherwise.
+                const uniqueUsers = suspectedWS.every((value, i, arr) => {
+                    return arr.filter((val) => {
+                        return value.committer.name === val.committer.name;
+                    }).length === 1;
+                });
+                // both conditions match
+                if (usersMatch && uniqueUsers) {
+                    match.push(true);
+                }
+
+            }
+        }
+    }
+    // If this is not exact it means that either one or more did not match
+    // or at least one matched multiple WSs meaning there are duplicates
+    if (match.length !== repo.topicSpaces.length) {
+        throw new Error("The wokspaces read from the repo don't match the" +
+            " info we read from the meta data");
+    }
+}
+/**
+ * By the time this gets called, we know that branches are unique and that they
+ * match.
+ * @param repo
+ * @param refs
+ */
+async function getThelatestState(
+    repo: GRepository,
+    refs: Branch[],
+): Promise<GRepository> {
+    let newRepo = repo;
+    for (const TS of repo.topicSpaces) {
+        for (const WS of TS.workSpaces) {
+            const foundRef = refs.find((value) => {
+                return value.name === WS.name;
+            });
+            // the tips don't match, meaning that what we read is behind
+            if (foundRef && (foundRef.tip.SHA !== WS.tip.SHA)) {
+                console.log("We found a tip that is behind: " + foundRef.name);
+                // They have the same name, we matched them based on that
+                const newWS = new WorkSpace(WS.name, foundRef.remoteUpstream, foundRef.tip,
+                    [foundRef.tip, ...WS.commits], WS.changeList, WS.originCommit);
+                newRepo = await changeWS(newRepo, TS, newWS);
+            }
+        }
+    }
+    return newRepo;
 }
