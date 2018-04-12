b5e28728a49342a35205921bae7ced2eb2879cc8
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-04-12 17:31:34 -0500

    Add the lookups util
    For now we will abstract the lookups we do since we might change the
    data structers from arrays to maps.

---
+++ src/util/lookups.ts
@@ -0,0 +1,60 @@
+import { GRepository } from "../model/app/g-repository";
+import { TopicSpace } from "../model/app/topicspace";
+import { WorkSpace } from "../model/app/workspace";
+
+/**
+ * A bunch of retrieval function to abstract our lookups. The main reason is
+ * we might change the underlying arrays to maps instead.
+ */
+
+/** Easy but nice to abstract. */
+export async function getWSfromTS(
+    repo: GRepository,
+    topicspaceName: string,
+    workspaceName: string,
+): Promise<WorkSpace> {
+    const foundTS = repo.topicSpaces.find((value) => {
+        return value.name === topicspaceName;
+    });
+    if (!foundTS) {
+        throw new Error("[getWSfromTS] cannot find the topicspace: " + topicspaceName);
+    }
+    const foundWS = foundTS.workSpaces.find((value) => {
+        return value.name === workspaceName;
+    });
+
+    if (!foundWS) {
+        throw new Error("[getWSfromTS] cannot find the workspace: " + workspaceName);
+    }
+
+    return foundWS;
+}
+/** Sometimes one doesn't know what topicspace they need to look in. */
+export async function getWS(
+    repo: GRepository,
+    workspaceName: string,
+): Promise<WorkSpace> {
+    for (const TS of repo.topicSpaces) {
+        const foundWS = TS.workSpaces.find((value) => {
+            return value.name === workspaceName;
+        });
+        if (foundWS) {
+            return foundWS;
+        }
+    }
+    throw new Error("[getWS] cannot find the workspace: " + workspaceName);
+}
+
+/** Again, just nice to abstract out. */
+export async function getTS(
+    repo: GRepository,
+    topicspaceName: string,
+): Promise<TopicSpace> {
+    const foundTS = repo.topicSpaces.find((value) => {
+        return value.name === topicspaceName;
+    });
+    if (!foundTS) {
+        throw new Error("[getTS] cannot find the topicspace: " + topicspaceName);
+    }
+    return foundTS;
+}
