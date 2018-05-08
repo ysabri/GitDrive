895532b47443e9db6ebb69c7b8b97fcbdab6914f
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-05-08 18:20:34 -0500

    Added a couple of state changes and fixed how state is mutated.
    Beforehand we could not mutate the state since everything was a
    readonly. The only thing we need to make sure mutates and is consistant
    is what we will be writting to desk, ie. appData. So that got fixed and
    there will probably be more things in appState that are private but not
    readonly, making them changable within the same instance of appState.
    appData is the only object we recreate in other words. The divide
    between the two might seem strange but dispatch might and will probably
    have to deal with more state things than appState and update them
    accordingly. We will see.

--- src/controller/app-state.ts
+++ src/controller/app-state.ts
@@ -1,11 +1,12 @@
 import { remote } from "electron";
 import { pathExistsSync, readFileSync } from "fs-extra";
 import { join } from "path";
+import { GRepository } from "../model/app/g-repository";
 import { AppData } from "../model/controller/appData";
 
 export class AppState {
-    public readonly appData: AppData;
     public readonly FirstTimer: boolean;
+    private appData: AppData;
 
     private readonly electronExePath: string = remote.app.getPath("exe");
     private readonly cachedDataPath: string = join(this.electronExePath, "/cache/cache.proto");
@@ -19,7 +20,22 @@ export class AppState {
         const buffer = readFileSync(this.cachedDataPath);
         this.appData = AppData.deserialize(new Uint8Array(buffer));
     }
+
+    public get currentRepo(): GRepository | undefined {
+        return this.appData.currentRepo;
+    }
+    public get repos(): Map<string, GRepository> {
+        return this.appData.repos;
+    }
+    public async changeSelectedRepo(
+        repo: GRepository,
+    ): Promise<void> {
+        this.appData.addRepo(repo);
+        this.appData = new AppData(repo, repo.users[0],
+            this.appData.repos);
+    }
     private isFirstTimer(): boolean {
         return !pathExistsSync(this.cachedDataPath);
     }
+
 }
--- src/controller/dispatcher.ts
+++ src/controller/dispatcher.ts
@@ -1,6 +1,9 @@
+import { ipcRenderer, OpenDialogOptions, remote } from "electron";
+import { loadGRepo } from "../git-drive/app/load-repo";
+import { GRepository } from "../model/app/g-repository";
 import { TopicSpace } from "../model/app/topicspace";
 import { AppState } from "./app-state";
-
+const changeTitle = "changeTitle";
 export class Dispatcher {
     private readonly appState: AppState;
 
@@ -15,11 +18,30 @@ export class Dispatcher {
     }
 
     public get TSs(): ReadonlyArray<TopicSpace> | undefined {
-        if (this.appState.appData.currentRepo) {
-            return this.appState.appData.currentRepo.topicSpaces;
+        if (this.appState.currentRepo) {
+            return this.appState.currentRepo.topicSpaces;
         } else {
             return undefined;
         }
+    }
+
+    public get currentRepo(): GRepository | undefined {
+        return this.appState.currentRepo;
+    }
+
+    public async loadNewRepo(): Promise<GRepository> {
+        // console.log("Made it into dispatcher, meaning we went all the way");
+        const options: OpenDialogOptions = {
+            properties: ["openDirectory"],
+        };
+        const files = await remote.dialog.showOpenDialog(options);
+        console.log(files[0]);
+        return await loadGRepo(files[0]);
+    }
+
+    public changeCurrRepo(repo: GRepository) {
+        ipcRenderer.send(changeTitle, repo.name);
+        this.appState.changeSelectedRepo(repo);
     }
 
 }
