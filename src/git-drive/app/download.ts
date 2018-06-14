66c56c294f64c8e6fbce580518048549562e1f22
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-06-13 19:32:52 -0500

    Added the creation of remote branches in the downlaod command
    This maybe works, I can't get clone to work to begin with. This is super
    frustraiting

--- src/git-drive/app/download.ts
+++ src/git-drive/app/download.ts
@@ -1,10 +1,27 @@
-import { clone } from "../git";
+import { Repository } from "../../model/git";
+// import { removeRemotePrefix } from "../../util";
+import { clone, createBranchFromRemote, getBranches } from "../git";
 
 export async function download(
     path: string,
     url: string,
 ): Promise<void> {
-
-
+    console.log("in download");
     await clone(url, path);
+    const repo: Repository = new Repository(path);
+    await createLocalCounterparts(repo);
+}
+
+
+async function createLocalCounterparts(
+    repo: Repository,
+): Promise<void> {
+    const refs = await getBranches(repo, "refs/remotes");
+    for (const ref of refs) {
+        // we don't want to create a local ΗEAD in our refs
+        if (ref.name !== "HEAD") {
+            await createBranchFromRemote(repo, ref.name,
+                "origin/" + ref.name);
+        }
+    }
 }
