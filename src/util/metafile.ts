61eb2da4dec0f2d906580e655b4d65f1577db44b
Yazeed Sabri <sabri.yazeed@gmail.com>
2018-06-14 18:07:08 -0500

    Fixed a bug with repo names
    So when we write the repo proto files we write the path and name of the
    repo. This is wrong because at least one of them will change upon
    someone else cloning and reading the repo. So what we do now is save the
    path as "-/-" and name will be as a result "-". We can abstract this away
    since we know the path we will write to or read the proto file from.
    This implies that the basename of a repo will always be the name of the
    repo, which is the case indeed.

--- src/util/metafile.ts
+++ src/util/metafile.ts
@@ -7,15 +7,17 @@ import { GRepository } from "../model/app";
 import { Repository } from "../model/git";
 
 export async function writeRepoInfo(repo: GRepository, path?: string): Promise<void> {
+    // The "-/-" is to trick the basename function into putting - as the name.
+    const toBeWriten = new GRepository("-/-", repo.topicSpaces, repo.users, repo.metaBranch);
     if (path) {
-        await writeFile(join(path, `${repo.name}.proto`), new Buffer(repo.serialize()), (err) => {
+        await writeFile(join(path, `${repo.name}.proto`), new Buffer(toBeWriten.serialize()), (err) => {
             if (err) {
                 throw err;
             }
             return;
         });
     } else {
-        await writeFile(join(repo.path, "repo.proto"), new Buffer(repo.serialize()), (err) => {
+        await writeFile(join(repo.path, "repo.proto"), new Buffer(toBeWriten.serialize()), (err) => {
             if (err) {
                 throw err;
             }
@@ -27,5 +29,8 @@ export async function writeRepoInfo(repo: GRepository, path?: string): Promise<v
 
 export async function readRepoInfo(repo: Repository): Promise<GRepository> {
     const buffer = await readFile(join(repo.path, "repo.proto"));
-    return GRepository.deserialize(new Uint8Array(buffer));
+    // The repo will have path as "-/-" and name as "-", we should fix this before
+    // returning it to the user.
+    const read = GRepository.deserialize(new Uint8Array(buffer));
+    return new GRepository(repo.path, read.topicSpaces, read.users, read.metaBranch);
 }
